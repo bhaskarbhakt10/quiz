495683d5159be94a033c9a3a77d0fba379f143d9
Bhaskar Bhakt <bhaskarbhakt10@gmail.com>
2023-06-03 11:18:14 +0530

    directory setup and assests

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+// config file maily for constants
+// paths 
+if (!defined("ROOT_PATH")) {
+    define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT'] . '/kids-quiz/');
+}
+if (!defined("ROOT_PATH_DB")) {
+    define("ROOT_PATH_DB", $_SERVER['DOCUMENT_ROOT'] . '/kids-quiz/database/config.database.php');
+}
+if (!defined("ROOT_PATH_DBTABLES")) {
+    define("ROOT_PATH_DBTABLES", $_SERVER['DOCUMENT_ROOT'] . '/kids-quiz/database/tables.database.php');
+}
+if (!defined("ROOT_PATH_CLASS")) {
+    define("ROOT_PATH_CLASS", $_SERVER['DOCUMENT_ROOT'] . '/kids-quiz/controller/');
+}
+//paths
+
+//urls
+if (!defined("ROOT_PATH_ACTION") || !defined("ROOT_URL_ACTION")) {
+    define("ROOT_PATH_ACTION", $_SERVER['DOCUMENT_ROOT'] . '/kids-quiz/classes/actions/');
+    $root_url = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/kids-quiz/classes/actions/';
+    define("ROOT_URL_ACTION", $root_url);
+}
+if (!defined("ROOT_URL")) {
+    $root_url = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/kids-quiz/';
+    define("ROOT_URL", $root_url);
+}
+
+if (!defined("ASSETS_URL")) {
+    $root_url = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/kids-quiz/assets/';
+    define("ASSETS_URL", $root_url);
+}
