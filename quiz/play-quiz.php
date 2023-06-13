f4c6b13d004c0355a84d688c52cec000a49a9647
dev2698 <dev@scaleup42.com>
2023-06-13 19:59:32 +0530

    Ui

--- quiz/play-quiz.php
+++ quiz/play-quiz.php
@@ -12,7 +12,17 @@ if ($play_quiz === true) {
     if ($questions_and_answers->num_rows > 0) {
 ?>
         <form action="" novalidate>
-            <div class="quiz-container">
+            <div class="quiz-container" id="test">
+
+                <!-- header logo  -->
+                <div class="quiz-logo">
+                    <img src="<?php echo ASSETS_URL ?>images/duk-quiz-logo.png" alt="">
+                </div>
+                <!-- header logo  -->
+
+                <div class="quiz-subheader">
+                    <img src="<?php echo ASSETS_URL ?>images/quiz-sub-header.png" alt="">
+                </div>
                 <div class="count">
                     <span id="current_question">1</span>/<span id="total_ques"></span>
                 </div>
@@ -38,8 +48,10 @@ if ($play_quiz === true) {
                                 ?>
                                         <div class="option_value" data-optCount="<?php echo $option_count = $option_count + 1 ?>">
                                             <div class="option_value_inner">
+                                           <div class="checkbox-container">
                                                 <span><?php echo chr(65 + $option_count - 1); ?>. </span>
-                                                <input type="checkbox" name="question_<?php echo $count; ?>" id="<?php echo $option; ?>" value="<?php echo $option; ?>" data-value="<?php echo chr(65 + $option_count - 1); ?>" <?php echo ($option_count === 1) ? 'required' : ''; ?>>
+                                                <input class="form-check-input" type="checkbox" name="question_<?php echo $count; ?>" id="<?php echo $option; ?>" value="<?php echo $option; ?>" data-value="<?php echo chr(65 + $option_count - 1); ?>" <?php echo ($option_count === 1) ? 'required' : ''; ?>>
+                                            </div>
                                                 <label for="<?php echo $option; ?>"><?php echo $option ?></label>
                                             </div>
                                         </div>
@@ -58,6 +70,20 @@ if ($play_quiz === true) {
                 </div>
             </div>
         </form>
+
+        <div class="quiz-footer">
+            <div class="py-2">
+                <div class="quiz-jesus-img">
+                    <img src="<?php echo ASSETS_URL ?>images/test.png" alt="">
+                </div>
+            </div>
+
+            <div class="duk-website row">
+                <a href="www.divineuk.org">www.divineuk.org</a>
+            </div>
+
+        </div>
+
 <?php
     }
 }
